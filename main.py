28504082fa839c5ff9094a2365f902b419042bbd
RevayDev <revaydev17@gmail.com>
2023-10-02 20:31:06 -0500

    version 1.0 esta es la base del codigo

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("hello word")
+import os
+import random
+
+os.system("cls")
+numero = random.randint(1,10)
+
+usuario = int(input("estoy pensando en un numero del 1 al 10 ¿cual es?: "))
+
+if numero == usuario:
+    print("si, ese era el numero")
+
+else:
+    print(f"ese no ere, era: {numero}")
